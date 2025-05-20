41433b0d48a885249db14381cda58454527bf280
Swarali Joshi <swarali.joshi@swarali-ltmnvue.internal.salesforce.com>
2025-05-20 12:03:31 +0530

    Fixed datetime issue

--- app/models/Task.py
+++ app/models/Task.py
@@ -1,13 +1,13 @@
-from datetime import date
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
 from app.enums.TaskStatus import Status
 
 class Task(BaseModel):
-    id: Optional[str] = Field(..., min_length=1)
+    id: Optional[str] = None
     title: str
     description: Optional[str] = None
     status: Status = Status.new
-    dueDate: date
+    dueDate: datetime 
     tags: Optional[List[str]]
